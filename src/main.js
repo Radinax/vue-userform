cc30a168bbfced8bf7cdde59a548f5837abe50a3
Adrian Beria <adrberia@gmail.com>
2018-07-18 13:55:51 -0400

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import Vuex from 'vuex'
+
+// Vue.use(Vuex)
 
 Vue.config.productionTip = false
-
+/*
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  }
+})
+*/
 new Vue({
   render: h => h(App)
 }).$mount('#app')
